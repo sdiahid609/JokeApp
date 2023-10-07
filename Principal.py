2d55d0fd826d7f4896ab36e088eb30e6f6be7654
sdiahid609 <xsdiahid609@ieshnosmachado.org>
2023-10-07 19:33:53 +0200

    translation improvement

--- Principal.py
+++ Principal.py
@@ -14,8 +14,10 @@ lang = input('Introduce el idioma:')
 #Comprobamos el idioma seleccionado
 if lang.lower() in ["español", "es", "spanish"]:
     url = 'https://v2.jokeapi.dev/joke/Any?lang=es'
+    es=True
 else:
     url = 'https://v2.jokeapi.dev/joke/Any?lang=en'
+    es=False
 
 #Guardamos la respuesta de la url en la variable joke
 joke = requests.get(url)
@@ -23,9 +25,11 @@ joke = requests.get(url)
 #Convertimos la variable joke en un diccionario usando json()
 joke = joke.json()
 
-#Ponemos el proveedor mymemory y indicamos los idiomas
-translator = Translator(provider='mymemory', from_lang='en', to_lang='es')
-#Guardamos la traducción en translation
-translation = translator.translate(textOutput())
-
-print (translation)
+if es==False:
+    #Ponemos el proveedor mymemory y indicamos los idiomas
+    translator = Translator(provider='mymemory', from_lang='en', to_lang='es')
+    #Guardamos la traducción en translation
+    translation = translator.translate(textOutput())
+    print (translation)
+else:
+    print(textOutput())
